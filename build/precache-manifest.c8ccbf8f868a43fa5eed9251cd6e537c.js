e2f17fa8715e7b40828821560d7d98840dd2b525
Muhammad Shiraz <61803642+MuhmmadSheraz@users.noreply.github.com>
2020-10-16 12:24:00 +0500

    application completed

---
+++ build/precache-manifest.c8ccbf8f868a43fa5eed9251cd6e537c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ef65009b5c24dabb17ec366e86350c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40a1f5865c6b8faab146",
+    "url": "/static/css/main.c664c759.chunk.css"
+  },
+  {
+    "revision": "8668a494161b2006130b",
+    "url": "/static/js/2.afcd96c6.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.afcd96c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40a1f5865c6b8faab146",
+    "url": "/static/js/main.56514b9a.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import TrackerView from "./View";
 import { GlobalProvider } from "./context/GlobalState";
+import firebase from "./Config/firebase";
+import axios from "axios";
 
 function App() {
+  const [tokens, setTokens] = useState<string>();
+
+  const callToken = async () => {
+    const messaging = firebase.messaging();
+    const token = await messaging.getToken();
+    setTokens(token);
+  };
+  useEffect(() => {
+    callToken();
+    {
+      tokens && checkNoti(tokens || "");
+    }
+
+    console.log("permision asked");
+    askForPermissioToReceiveNotifications();
+    return () => {
+      checkNoti(tokens || "");
+      // checkNoti(token);
+    };
+  }, [tokens]);
+  const askForPermissioToReceiveNotifications = async () => {
+    try {
+      const messaging = firebase.messaging();
+      await messaging.requestPermission();
+      const token = await messaging.getToken();
+      // getToken(token);
+      return token;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const checkNoti = (token: string) => {
+    let body = {
+      notification: {
+        title: "Expense Tracker  App ",
+        body: "Having trouble expense Check Here...",
+        icon: "http://url-to-an-icon/icon.png",
+      },
+      to: token,
+    };
+    axios({
+      method: "post",
+      url: "https://fcm.googleapis.com/fcm/send",
+      data: body,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "key=AAAAjRpuUaM:APA91bHqdK-fW3o4xZdd1qRSy9AMXdyn_nTVl53HOWp2BkcKuvvx9Aqsfv8n-AxFJ7vWxZBdAJiCBI0Brk9vvmfHDQrmacV6_QZl8KQ9DECYe06_FWGl3BqQsVojJAPDS5PE-di_QVKD",
+      },
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
   return (
     <GlobalProvider>
       <div className="App-header">
